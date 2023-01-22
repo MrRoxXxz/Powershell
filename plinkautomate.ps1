e24c8fe7070dabbce6299a360eb14ad69216b7fb
William G <60862046+MrRoxXxz@users.noreply.github.com>
2023-01-22 12:01:10 -0600

    Add files via upload

--- plinkautomate.ps1
+++ plinkautomate.ps1
@@ -1,6 +1,3 @@
-# Allow PS1 to run
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process
-
 # Search for Plink.exe in the "program files"\PuTTY directory
 $path = "C:\Program Files\"
 $plinkExe = Get-ChildItem -Path $path -Filter "Plink.exe" -Recurse
@@ -20,7 +17,7 @@ Write-Host "COM port in use: $com"
 	}
 
 # Create list of commands in commands.txt
-$commands = "", "enable", "term len 0", "show version", "show interfaces description","show interfaces trunk","show ip interface","show mac address-table","show ip arp","show vlan","show cdp neighbor detail", "show spanning-tree detail", "show run" ,"exit"
+$commands = "", "enable", "term len 0", "show version", "show interfaces description", "show interfaces", "show interfaces trunk","show ip interface","show mac address-table","show ip arp","show vlan","show cdp neighbor detail", "show spanning-tree detail", "show run" ,"exit"
 $commands | Out-File -FilePath "commands.txt"
 
 #Create output filename
@@ -33,7 +30,6 @@ foreach ($command in $commands) {Write-Output "Running command: $command"}
 #Set Timer
 Write-Host "Printing to $filename, Please wait 10 seconds and then use ^C to exit"
 
-
 # Use commands.txt as input to plink.exe
 cmd /C "type commands.txt | plink.exe -serial $com -sercfg 9600,8,n,1,N" | Out-File "$filename.txt" -Append
- return
+	
